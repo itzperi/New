1e064aa8f955c7e59d8de6b3f367501022899b91
Periyanan <itzmeperi@gmail.com>
2025-12-20 22:35:04 +0530

    UI

--- components/ui/logo-cloud-4.tsx
+++ components/ui/logo-cloud-4.tsx
@@ -17,16 +17,16 @@ type LogoCloudProps = React.ComponentProps<"div"> & {
 export function LogoCloud({ logos }: LogoCloudProps) {
   return (
     <div className="relative mx-auto max-w-5xl py-12">
-      <div className="-translate-x-1/2 -top-px pointer-events-none absolute left-1/2 w-screen border-t border-white/5" />
+      <div className="-translate-x-1/2 -top-px pointer-events-none absolute left-1/2 w-full border-t border-white/5" />
 
       <InfiniteSlider gap={60} reverse duration={40} durationOnHover={80}>
         {logos.map((logo) => (
-          <div key={`logo-${logo.alt}`} className="flex items-center justify-center grayscale opacity-50 hover:grayscale-0 hover:opacity-100 transition-all duration-300">
-             <img
-                alt={logo.alt}
-                className="h-8 md:h-12 w-auto select-none brightness-0 invert"
-                loading="lazy"
-                src={logo.src}
+          <div key={`logo-${logo.alt}`} className="flex items-center justify-center">
+            <img
+              alt={logo.alt}
+              className="h-10 md:h-12 w-auto select-none"
+              loading="lazy"
+              src={logo.src}
             />
           </div>
         ))}
@@ -34,16 +34,16 @@ export function LogoCloud({ logos }: LogoCloudProps) {
 
       <ProgressiveBlur
         blurIntensity={2}
-        className="pointer-events-none absolute top-0 left-0 h-full w-[200px] z-10"
+        className="pointer-events-none absolute top-0 left-0 h-full w-[15%] min-w-[50px] max-w-[200px] z-10"
         direction="left"
       />
       <ProgressiveBlur
         blurIntensity={2}
-        className="pointer-events-none absolute top-0 right-0 h-full w-[200px] z-10"
+        className="pointer-events-none absolute top-0 right-0 h-full w-[15%] min-w-[50px] max-w-[200px] z-10"
         direction="right"
       />
 
-      <div className="-translate-x-1/2 -bottom-px pointer-events-none absolute left-1/2 w-screen border-b border-white/5" />
+      <div className="-translate-x-1/2 -bottom-px pointer-events-none absolute left-1/2 w-full border-b border-white/5" />
     </div>
   );
 }
